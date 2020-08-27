a1c6f4c94b72e075562c634876799cf485dc432e
selam lemma <oneiconforlife26@gmail.com>
2020-08-27 01:11:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 # Write your code here.
 def line
-  "The line is currently empty."
+#  "The line is currently empty."
 end
 
 def take_a_number("katz_deli","name")
-take_a_number.each do |take_a_number|
-  number = 1
+#take_a_number.each do |take_a_number|
+#  number = 1
     ## "welcome,#{person_name}.you are {|number+=1|in line}."
 end
 
 def now_serving
-  "There is nobody waiting to be served!"
+#  "There is nobody waiting to be served!"
 end
