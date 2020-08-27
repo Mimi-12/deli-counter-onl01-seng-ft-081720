d2a0c814eb71e660ddcdff209051d48b7921964f
selam lemma <oneiconforlife26@gmail.com>
2020-08-27 01:02:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def line ("current_place")
+  "The line is currently empty."
+end
+
+def take_a_number("katz_deli","person_name")
+  number = 1
+     "welcome,#{person_name}.you are {|number+=1|in line}."
+end
+
+def now_serving
+if (next_person_in_line)
+  puts "remove them from the front"
+elsif
+  "There is nobody waiting to be served!"
+end
+end
